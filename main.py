18c32928e1ca8b0544541db476995457a59be157
Muhammad-javed440 <meherjaved440@gmail.com>
2025-01-04 00:20:49 +0500

    fastapi done

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app =FastAPI() 
+
+@app.get('/javed')
+async def javed() -> dict[str, str]:
+    return{"messege":"hello javed"}   
+
+@app.get('/about')
+async def javed1() -> str:
+    return "hello javed1"
+
+@app.get('/contact')
+async def contact() -> str:
+    return "hello contact me at 8vS6f@example.com"
